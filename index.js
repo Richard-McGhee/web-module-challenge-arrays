366719bfd07ed12c9ce5ab7d9a356c06663a28d0
richard-mcghee <richardjmcghee@yahoo.com>
2020-06-10 14:21:40 -0400

    completed tasks, might work on stretch

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ function copy(oldArr, newArr){
     return console.log(newArr);
 }
 
-console.log(originalFlavors, 'poppingFlavors');
+copy(originalFlavors, 'poppingFlavors');
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -155,13 +155,17 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
-
+function filterByWord(arr, flavor, newArrName){
+    newArrName = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].includes(flavor)){
+           newArrName.push(arr[i]); 
+        }
+    }
+    return newArrName;
 }
 
-
+console.log(filterByWord(originalFlavors, 'Chocolate', 'holidayFavorites'));
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
 
